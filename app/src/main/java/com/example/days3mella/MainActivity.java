aa46847e0d738150a46b4d439e16b3e604137b0b
mellaarfina <12050122345@students.uin-suska.ac.ic>
2022-04-22 09:36:50 +0700

    commit

---
+++ app/src/main/java/com/example/days3mella/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.days3mella;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private View view;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tampilan_menu);
+    }
+
+    public void clickRS(View view) {
+        //gambar RS diclick
+        Intent i = new Intent(this,RS.class);
+        startActivity(i);
+    }
+
+}
